0122b6feb634940a1684354ae4b0cd416bb1b632
Daniel Hultgren <daniel.cf.hultgren@gmail.com>
2019-03-10 10:09:30 +0100

    Added a lil release helper batchy

---
+++ MakeRelease.bat
@@ -0,0 +1,9 @@
+﻿@echo off
+
+rmdir /s /q "gm_discord_rpc_release"
+mkdir "gm_discord_rpc_release"
+mkdir "gm_discord_rpc_release\add-to-lua-bin-folder"
+mkdir "gm_discord_rpc_release\add-to-common-garrysmod-folder"
+xcopy /s/y/i "build\gmcl_discord_rpc_win32.dll" "gm_discord_rpc_release\add-to-lua-bin-folder"
+xcopy /s/y/i "lib\discord-rpc.dll" "gm_discord_rpc_release\add-to-common-garrysmod-folder"
+xcopy /s/y/i "lib\send-presence.exe" "gm_discord_rpc_release\add-to-common-garrysmod-folder"
